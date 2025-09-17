8dc6ded43faa0366b7b2d1624cf0cd9669c6a3f3
mirellaliraa <mirellalira0801@gmail.com>
2025-09-17 16:57:47 -0300

    17-09-2025

---
+++ Firebase/cine_favorite/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Filmes'),
+        ),
+      ),
+    );
+  }
+}
+
